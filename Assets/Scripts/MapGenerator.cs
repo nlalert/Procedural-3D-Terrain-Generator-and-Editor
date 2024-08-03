d17c20049cac40219e3cf1d455a66a73356a9bd0
Natthakrit Lalert <99980994+nlalert@users.noreply.github.com>
2024-08-03 18:47:13 +0700

    initial

---
+++ Assets/Scripts/MapGenerator.cs
@@ -0,0 +1,87 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGenerator : MonoBehaviour {
+    public enum DrawMode {NoiseMap, ColorMap, Mesh};
+    public DrawMode drawMode;
+
+    public const int mapChunkSize = 241;//vertices
+    [Range(0, 6)]
+    public int levelOfDetail;
+    public float noiseScale;
+
+    public int octaves;
+    [Range(0, 1)]
+    public float persistance;
+    public float lacunarity;
+
+    public int seed;
+    public Vector2 offset;
+    
+    public float meshHeightMultiplier;
+    public AnimationCurve meshHeightCurve;
+
+    public bool autoUpdate;
+
+    public TerrainType[] regions;
+
+    public void DrawMapInEditor(){
+        MapData mapData = GenerateMap();
+        MapDisplay display = FindObjectOfType<MapDisplay>();
+        if(drawMode == DrawMode.NoiseMap){
+            display.DrawTexture(TextureGenerator.TextureFromHeightMap(mapData.heightMap));
+        }
+        else if(drawMode == DrawMode.ColorMap){
+            display.DrawTexture(TextureGenerator.TextureFromColorMap(mapData.colorMap, mapChunkSize, mapChunkSize));
+        }
+        else if(drawMode == DrawMode.Mesh){
+            display.DrawMesh(MeshGenerator.GenerateTerrainMesh(mapData.heightMap, meshHeightMultiplier, meshHeightCurve, levelOfDetail), TextureGenerator.TextureFromColorMap(mapData.colorMap, mapChunkSize, mapChunkSize));
+        }
+    }
+
+    MapData GenerateMap(){
+        float[,] noiseMap = Noise.GenerateNoiseMap(mapChunkSize, mapChunkSize, seed, noiseScale, octaves, persistance, lacunarity, offset);
+
+        Color[] colorMap = new Color[mapChunkSize * mapChunkSize];
+        for (int x = 0; x < mapChunkSize; x++){
+            for (int y = 0; y < mapChunkSize; y++){
+
+                float currentHeight = noiseMap[x, y];
+
+                for (int i = 0; i < regions.Length; i++) {
+                    if(currentHeight <= regions[i].height){
+                        colorMap[y * mapChunkSize + x] = regions[i].color;
+                        break;
+                    }
+                }
+            }
+        }
+
+        return new MapData(noiseMap, colorMap);
+    }
+
+    void OnValidate(){
+        if(noiseScale < 0) noiseScale = 0.0001f;;
+        if(lacunarity < 1) lacunarity = 1;
+        if(octaves < 0) octaves = 0;
+    }
+}
+
+[System.Serializable]
+public struct TerrainType {
+    public String name;
+    public float height;
+    public Color color;
+}
+
+public struct MapData {
+    public float[,] heightMap;
+    public Color[] colorMap;
+
+    public MapData(float[,] heightMap, Color[] colorMap) {
+        this.heightMap = heightMap;
+        this.colorMap = colorMap;
+    }
+}
---
+++ Assets/Scripts/MeshGenerator.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class MeshGenerator {
+    public static MeshData GenerateTerrainMesh(float[,] heightMap, float heightMultiplier, AnimationCurve heightCurve, int levelOfDetail)
+    {
+        int width = heightMap.GetLength(0);
+        int height = heightMap.GetLength(1);
+        float halfWidth = (width - 1) / 2f;
+        float halfHeight = (height - 1) / 2f;
+
+        int meshSimplificationIncrement = (levelOfDetail == 0) ? 1 : levelOfDetail * 2;
+        int verticesPerLine = (width - 1)/meshSimplificationIncrement + 1;
+
+        MeshData meshData = new MeshData(verticesPerLine, verticesPerLine);
+        int vertexIndex = 0;
+
+        for (int x = 0; x < width; x += meshSimplificationIncrement) {
+            for (int y = 0; y < height; y += meshSimplificationIncrement) {
+                // meshData.vertices[vertexIndex] = new Vector3(x - halfWidth, heightCurve.Evaluate(heightMap[x, y]) * heightMultiplier, y - halfHeight);
+                // meshData.uvs[vertexIndex] = new Vector2(x/(float)width, y/(float)height);
+                meshData.vertices[vertexIndex] = new Vector3(x - halfWidth, heightCurve.Evaluate(heightMap[width-1-x, height-1-y]) * heightMultiplier, y - halfHeight);
+                meshData.uvs[vertexIndex] = new Vector2(1f - x/(float)width, 1f - y/(float)height);
+
+                if(x < width -1 && y < height - 1) {
+                    meshData.AddTriangle(vertexIndex, vertexIndex + verticesPerLine + 1, vertexIndex + verticesPerLine);
+                    meshData.AddTriangle(vertexIndex, vertexIndex + 1, vertexIndex + verticesPerLine + 1);
+                }
+
+                vertexIndex++;
+            } 
+        }
+        
+        return meshData;
+    }
+}
+
+public class MeshData {
+    public Vector3[] vertices;
+    public int[] triangles;
+    public Vector2[] uvs;
+    int triangleIndex;
+
+    public MeshData(int meshWidth, int meshHeight) {
+        vertices    = new Vector3[meshWidth * meshHeight];
+        uvs          = new Vector2[meshWidth * meshHeight];
+        triangles   = new int[(meshWidth - 1) * (meshHeight - 1) * 6];
+    }
+
+    public void AddTriangle(int a, int b, int c) {
+        triangles[triangleIndex]     = a;
+        triangles[triangleIndex + 1] = b;
+        triangles[triangleIndex + 2] = c;
+        triangleIndex += 3;
+    }
+
+    public Mesh CreateMesh() {
+        Mesh mesh = new Mesh();
+        mesh.vertices = this.vertices;
+        mesh.triangles = this.triangles;
+        mesh.uv = this.uvs;
+        mesh.RecalculateNormals();
+
+        return mesh;
+    }
+}
