935674548bdabe9588a4b8fac861e36b5939ceba
Natthakrit Lalert <99980994+nlalert@users.noreply.github.com>
2024-10-21 18:27:18 +0700

    refactor MapPreview, remove MapGenerator Class

--- Assets/Editors/MapPreviewEditor.cs
+++ Assets/Editors/MapPreviewEditor.cs
@@ -3,20 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 
-[CustomEditor (typeof(MapGenerator))]
-public class MapGeneratorEditor : Editor
+[CustomEditor (typeof(MapPreview))]
+public class MapPreviewEditor : Editor
 {
     public override void OnInspectorGUI() {
-        MapGenerator mapGen = (MapGenerator)target;
+        MapPreview mapPreview = (MapPreview)target;
 
         if(DrawDefaultInspector()) {// if value value was changed
-            if(mapGen.autoUpdate) {
-                mapGen.DrawMapInEditor();
+            if(mapPreview.autoUpdate) {
+                mapPreview.DrawMapInEditor();
             }
         }
 
         if(GUILayout.Button("Generate")) {
-            mapGen.DrawMapInEditor();
+            mapPreview.DrawMapInEditor();
         }
     }
 }
---
+++ Assets/Scripts/MapPreview.cs
@@ -0,0 +1,83 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class MapPreview : MonoBehaviour
+{
+    public Renderer textureRenderer;
+    public MeshFilter meshFilter;
+    public MeshRenderer meshRenderer;
+
+    public enum DrawMode {NoiseMap, Mesh, FalloffMap};
+    public DrawMode drawMode;
+
+    public MeshSettings meshSettings;
+    public HeightMapSettings heightMapSettings;
+    public textureData textureData;
+
+    public Material terrainMaterial;
+
+    [Range(0, MeshSettings.numSupportedLODS-1)]
+    public int editorPreviewLOD;
+
+    public bool autoUpdate;
+
+    public void DrawMapInEditor(){
+        textureData.ApplyToMaterial(terrainMaterial);
+        textureData.UpdateMeshHeights(terrainMaterial, heightMapSettings.minHeight, heightMapSettings.maxHeight);
+        HeightMap heightMap = HeightMapGenerator.GenerateHeightMap(meshSettings.numVertsPerLine, meshSettings.numVertsPerLine, heightMapSettings, Vector2.zero);
+        if(drawMode == DrawMode.NoiseMap){
+            DrawTexture(TextureGenerator.TextureFromHeightMap(heightMap));
+        }
+        else if(drawMode == DrawMode.Mesh){
+            DrawMesh(MeshGenerator.GenerateTerrainMesh(heightMap.values, meshSettings, editorPreviewLOD));
+        }
+        else if(drawMode == DrawMode.FalloffMap){
+            DrawTexture(TextureGenerator.TextureFromHeightMap(new HeightMap(FalloffGenerator.GenerateFalloffMap(meshSettings.numVertsPerLine), 0, 1)));
+        }
+    }
+
+    public void DrawTexture(Texture2D texture) {
+        //don't want to go game mode
+        textureRenderer.sharedMaterial.mainTexture = texture;
+        textureRenderer.transform.localScale = new Vector3(texture.width / 10f, 1, texture.height / 10f);
+
+        textureRenderer.gameObject.SetActive(true);
+        meshFilter.gameObject.SetActive(false);
+    }
+
+    public void DrawMesh(MeshData meshData) {
+        meshFilter.sharedMesh = meshData.CreateMesh();
+
+        textureRenderer.gameObject.SetActive(false);
+        meshFilter.gameObject.SetActive(true);
+    }
+    
+    void OnValuesUpdated(){
+        if (!Application.isPlaying) { // In Editor Mode
+            EditorApplication.delayCall += () => DrawMapInEditor(); // Postpone execution
+        }
+    }
+
+    void OnTextureValuesUpdated(){
+        textureData.ApplyToMaterial(terrainMaterial);
+    }
+
+    void OnValidate(){
+        //subscribe
+        if(meshSettings != null){
+            meshSettings.OnValuesUpdated -= OnValuesUpdated;
+            meshSettings.OnValuesUpdated += OnValuesUpdated;
+        }
+        if(heightMapSettings != null){
+            heightMapSettings.OnValuesUpdated -= OnValuesUpdated;
+            heightMapSettings.OnValuesUpdated += OnValuesUpdated;
+        }
+        if(textureData != null){
+            textureData.OnValuesUpdated -= OnTextureValuesUpdated;
+            textureData.OnValuesUpdated += OnTextureValuesUpdated;
+        }
+    }
+}
